625f9090091edc3a4b83350557ff26e54470bf41
firlandofarel27 <firlandofarel27@gmail.com>
2026-09-11 08:17:12 +0700

    rectangle

---
+++ src/id/ac/polinema/Rectangle.java
@@ -0,0 +1,19 @@
+package src.id.ac.polinema;
+
+public class Rectangle {
+    int width;
+    int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int area() {
+        return width * height;
+    }
+
+    public int perimeter() {
+        return 2 * (width + height);
+    }
+}
