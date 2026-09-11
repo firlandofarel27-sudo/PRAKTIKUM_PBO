5c66269be870ee044913847a96695d891bbdcc26
firlandofarel27 <firlandofarel27@gmail.com>
2026-09-11 08:55:08 +0700

    Motor modifikasi

--- MotorEncapsulation/motor.java
+++ MotorEncapsulation/motor.java
@@ -4,13 +4,35 @@ public class motor {
     public int kecepatan = 0;
     public boolean kontakOn = false;
 
-    public void printStatus() {
+    public void nyalakanMesin() {
+        kontakOn = true;
+    }
+    public void matikanMesin(){
+        kontakOn = false;
+        kecepatan = 0;
+    }
+    public void tambahKecepatan(){
+        if(kontakOn == true){
+            kecepatan += 5;
+        }
+        else{
+            System.out.println("Kecepatan tidak bisa bertambah karena mesin OFF! \n");
+        }
+    }
+    public void kurangiKecepatan(){
+        if(kontakOn == true){
+            kecepatan -= 5;
+        }
+        else{
+            System.out.println("Kecepatan tidak bisa berkurang karena mesin OFF! \n");
+        }
+    }
+    public void printStatus(){
         if (kontakOn == true) {
             System.out.println("Kontak ON");
         }
-        else {
+        else{
             System.out.println("Kontak OFF");
         }
-        System.out.println("Kecepatan"+ kecepatan+"\n");
     }
 }
