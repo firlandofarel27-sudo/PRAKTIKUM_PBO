423b50d77ff5755e5dde5af2266e81f3871f10c7
firlandofarel27 <firlandofarel27@gmail.com>
2026-09-19 21:12:55 +0700

    Laptop Percobaan 1

---
+++ src/id/ac/polinema/relasiclass/Laptop.java
@@ -0,0 +1,29 @@
+package src.id.ac.polinema.relasiclass;
+
+public class Laptop {
+    private String merk;
+    private Processor proc;
+
+    public Laptop() {
+    }
+    public Laptop(String merk, Processor proc) {
+        this.merk = merk;
+        this.proc = proc;
+    }
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+    public String getMerk() {
+        return merk;
+    }
+    public void setProc(Processor proc) {
+        this.proc = proc;
+    }
+    public Processor getProc() {
+        return proc;
+    }
+    public void info() {
+        System.out.println("Merk laptop = "+ merk);
+        proc.info();
+    }
+}
